870d14d48c0ef48b1727b17dd8b8b84197a84f19
wuzhenyong <young_zya@163.com>
2023-08-30 14:13:02 +0800

    FEAT-测试

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/api/openai/', // 重定向到你的函数的 API 路径
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
